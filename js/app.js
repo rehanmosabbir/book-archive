3c4a15b8c2f061031b66a234d1f850aacf556e6e
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-09-02 07:49:14 +0600

    fix some bugs

--- js/app.js
+++ js/app.js
@@ -12,23 +12,25 @@ document.getElementById("search-btn").addEventListener("click", loadBookData);
 const displayBookData = (data) => {
   const books = data.docs;
   const numBooksContainer = document.getElementById("num-books");
-  const h3 = document.createElement("h3");
-  h3.classList.add("text-danger", "fw-bold");
-  h3.innerText = `Found : ${data.numFound} books`;
-  numBooksContainer.appendChild(h3);
+  const h4 = document.createElement("h4");
+  h4.classList.add("text-danger", "fw-bold", "my-4");
+  h4.innerText = `${data.numFound === 0 ? `No result found!` : `Found: ${data.numFound} books`}`;
+  numBooksContainer.appendChild(h4);
   books.forEach((book) => {
     console.log(book);
     const bookContainer = document.getElementById("book-container");
     const div = document.createElement("div");
     div.classList.add("col-md-3");
     div.innerHTML = `
-        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <div class="text-dark mb-3" style="max-width: 18rem; border: 1px solid #222">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-thumbnail" style="width: 18rem;" alt="${
+      book.title
+    }">
             <div class="card-header">${book.title}</div>
             <div class="card-body">
-                <h5 class="card-title">${[...book.author_name]}</h5>
-                <p class="card-text">${book.publish_date[0]}</p>
-                <p class="card-text">${[...book.publisher]}</p>
+                <h5 class="card-title">Author name : ${book?.author_name?.length > 0 ? [...book.author_name] : "N/A"}</h5>
+                <p class="card-text">First Publish year : ${book?.first_publish_year}</p>
+                <p class="card-text">Publisher : ${book?.publisher?.length > 0 ? [...book.publisher] : "N/A"}</p>
             </div>
         </div>
     `;
